f02ac7d0c98f062e8652ae1da7eee1bff14fae51
paula <paulamartyna1@gmail.com>
2021-04-10 20:09:20 +0200

    setting i ship

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import sys
 import pygame
+from setting import Settings
+from ship import Ship
 
 def run_game():
+    #inicializacja pygame
     pygame.init()
-    screen = pygame.display.set_mode((800,500))
+    al_settings = Settings()
+    #ustawienie szerokości i wysokości okna
+    screen = pygame.display.set_mode((al_settings.screen_width, al_settings.screen_height))
+    #tekst, który wyśwteila się jako tytuł gry
     pygame.display.set_caption("Alien Invasion!")
+    #ustawienie koloru tła
+    bg_color = (al_settings.bg_color)
+    # wstawiam shipa
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(bg_color)
+        ship.blitme()
         pygame.display.flip()
 run_game()
 print("Ahoj Paula, tu Krecik")
---
+++ setting.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Store all settings here"""
+
+    def __init__(self):
+        #initializa the game settings
+        #Screemn settings
+        self.screen_width = 800
+        self.screen_height = 500
+        self.bg_color = (0,100,250)
