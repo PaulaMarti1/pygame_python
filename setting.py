782ce75066dca44a0944b30c5999e35adbeebccd
paula <paulamartyna1@gmail.com>
2021-04-10 20:15:08 +0200

    komentarz

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@ class Settings():
 
     def __init__(self):
         #initializa the game settings
-        #Screemn settings
+        #Screen settings
         self.screen_width = 800
         self.screen_height = 500
         self.bg_color = (0,100,250)
